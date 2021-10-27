cdd71cc8d80993ebc7a078eb5617396ec6daa25c
Powarox219 <moriniere.robin1877@gmail.com>
2021-10-27 16:18:12 +0200

    git init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"/>
+    <div id="app">
+        <grille/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Grille from './components/Grille.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Grille
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+      font-family: Avenir, Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 60px;
+    }
+
+    #app {
+        margin: auto;
+    }
 </style>
---
+++ src/components/Grille.vue
@@ -0,0 +1,177 @@
+<template>
+    <h1>SnowMan Game</h1>
+
+    <div class="grid">
+        <div class="elem" v-for="elem in grid" v-bind:key="elem.id">
+        </div>
+    </div>
+
+    <div class="moove">
+        <div id="top_btn">
+            <button v-on:click="moove_player('up')">
+                <i class="fas fa-arrow-up"></i>  Haut  <i class="fas fa-arrow-up"></i>
+            </button>
+        </div>
+        <div id="bot_btn">
+            <button v-on:click="moove_player('left')">
+                <i class="fas fa-arrow-left"></i>  Gauche  <i class="fas fa-arrow-left"></i>
+            </button>
+            <button v-on:click="moove_player('down')">
+                <i class="fas fa-arrow-down"></i>  Bas  <i class="fas fa-arrow-down"></i>
+            </button>
+            <button v-on:click="moove_player('right')">
+                <i class="fas fa-arrow-right"></i>  Droite  <i class="fas fa-arrow-right"></i>
+            </button>
+        </div>
+    </div>
+
+    <button @keyup.left="moove_player('left')" @keyup.up="moove_player('up')" @keyup.down="moove_player('down')" @keyup.right="moove_player('right')">clavier</button>
+
+    <button @click="test_moove()">test</button>
+</template>
+
+<script>
+    export default {
+        name: "Grille",
+        methods: {
+            moove_player(side) {
+                console.log(side);
+            },
+
+            test_moove() {
+                this.grid[0].state = 'player';
+                this.grid[3].state = 'none';
+                console.log(this.grid);
+                this.build_grid;
+            }
+        },
+        computed: {
+            build_grid() {
+                let grid_div = document.querySelector('.grid');
+                let all_elem = grid_div.querySelectorAll('.elem');
+
+                for(let i = 0; i < all_elem.length; i++){
+                    all_elem[i].classList.add(this.grid[i].state);
+                }
+                return this.grid;
+            }
+        },
+        data() {
+            return {
+                test: "ici",
+                grid: [{
+                        id: 0,
+                        state: 'none'
+                    },
+                    {
+                        id: 1,
+                        state: 'top'
+                    },
+                    {
+                        id: 2,
+                        state: 'none'
+                    },
+                    {
+                        id: 3,
+                        state: 'player'
+                    },
+                    {
+                        id: 4,
+                        state: 'center'
+                    },
+                    {
+                        id: 5,
+                        state: 'none'
+                    },
+                    {
+                        id: 6,
+                        state: 'none'
+                    },
+                    {
+                        id: 7,
+                        state: 'bottom'
+                    },
+                    {
+                        id: 8,
+                        state: 'none'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+    .grid {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        /* margin: 0 300px; */
+    }
+
+    .elem {
+        width: 200px;
+        height: 200px;
+        background: #777;
+        border-radius: 5px;
+        margin: 5px;
+        background-image: url('../assets/nothing.png');
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+
+    .moove div button {
+        width: 100px;
+        padding: 5px;
+        margin: 2px;
+        background: #333;
+        color: #DDD;
+        border: 2px solid #333;
+        border-radius: 2px;
+        font-weight: bold;
+        cursor: pointer;
+    }
+
+    /* .moove #top_btn {
+        display: flex;
+        justify-content: center;
+    }
+
+    .moove #bot_btn {
+        display: flex;
+        justify-content: center;
+    } */
+
+    .top {
+        background-image: url('../assets/top.png');
+    }
+
+    .center {
+        background-image: url('../assets/center.png');
+    }
+
+    .bottom {
+        background-image: url('../assets/bottom.png');
+    }
+
+    .player {
+        background-image: url('../assets/player.png');
+    }
+
+    .succes {
+        background-image: url('../assets/all.png');
+    }
+
+    .fail1 {
+        background-image: url('../assets/fail1.png');
+    }
+
+    .fail2 {
+        background-image: url('../assets/fail2.png');
+    }
+
+    .fail3 {
+        background-image: url('../assets/fail3.png');
+    }
+
+</style>
