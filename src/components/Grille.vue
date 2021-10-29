a4e2eceddc883d31ffa986fe472f350a09393cc5
Powarox <firerobinice1877@gmail.com>
2021-10-29 19:26:36 +0200

    Upgrade stardog query

--- src/components/Grille.vue
+++ src/components/Grille.vue
@@ -1,10 +1,13 @@
 <template>
     <h1>SnowMan Game</h1>
 
-    <div class="grid">
-        <div class="elem" v-for="elem in grid" v-bind:key="elem.id">
+    <section class="grid">
+        <div class="rows" v-for="elem in create_grid()" v-bind:key="elem.id">
+            <div class="elem" v-for="e in elem" v-bind:key="e.id">
+
+            </div>
         </div>
-    </div>
+    </section>
 
     <div class="moove">
         <div id="top_btn">
@@ -12,27 +15,49 @@
                 <i class="fas fa-arrow-up"></i>  Haut  <i class="fas fa-arrow-up"></i>
             </button>
         </div>
-        <div id="bot_btn">
+
+        <div id="center_btn">
             <button v-on:click="moove_player('left')">
                 <i class="fas fa-arrow-left"></i>  Gauche  <i class="fas fa-arrow-left"></i>
             </button>
-            <button v-on:click="moove_player('down')">
-                <i class="fas fa-arrow-down"></i>  Bas  <i class="fas fa-arrow-down"></i>
+
+            <button @keyup.left="moove_player('left')" @keyup.up="moove_player('up')"
+                @keyup.down="moove_player('down')" @keyup.right="moove_player('right')">Clavier
             </button>
+
             <button v-on:click="moove_player('right')">
                 <i class="fas fa-arrow-right"></i>  Droite  <i class="fas fa-arrow-right"></i>
             </button>
         </div>
+
+        <div id="down_btn">
+            <button v-on:click="moove_player('down')">
+                <i class="fas fa-arrow-down"></i>  Bas  <i class="fas fa-arrow-down"></i>
+            </button>
+        </div>
     </div>
 
-    <button @keyup.left="moove_player('left')" @keyup.up="moove_player('up')" @keyup.down="moove_player('down')" @keyup.right="moove_player('right')">clavier</button>
+    <br><br>
 
     <button @click="test_moove()">test</button>
 
-    <button @click="stardog()">Stardog</button>
+    <button @click="create_grid()">Create Grid</button>
+
+    <button @click="build_grid()">Build Grid</button>
+
+    <button @click="build_grid()">Start Game</button>
+
 </template>
 
 <script>
+    const { Connection, query } = require('stardog');
+
+    const conn = new Connection({
+        username: 'admin',
+        password: 'admin',
+        endpoint: 'http://localhost:5820',
+    });
+
     export default {
         name: "Grille",
         methods: {
@@ -41,106 +66,73 @@
             },
 
             test_moove() {
-                this.grid[0].state = 'player';
-                this.grid[3].state = 'none';
                 console.log(this.grid);
-                this.build_grid;
             },
 
-            stardog(){
-                const { Connection, query } = require('stardog');
+            create_grid(){
+                let grid = [];
+                let rows = 10;
 
-                const conn = new Connection({
-                    username: 'admin',
-                    password: 'admin',
-                    endpoint: 'http://localhost:5820',
-                });
-
-                let request = 'SELECT ?Player ?X ?Y WHERE{ ?Player a grid:PlayerCell . ?Player grid:X ?X . ?Player grid:Y ?Y .}';
-
-                /*"""
-                SELECT ?Cell WHERE {?Cell rdf:type grid:PlayerCell.}
-
-                // Return le joueur et sa pos X et Y
-                SELECT ?Player ?X ?Y
-                WHERE {
-                    ?Player a grid:PlayerCell .
-                    ?Player grid:X ?X .
-                    ?Player grid:Y ?Y .
+                for(let i = 0; i < rows; i++){
+                    grid.push([]);
+                    grid[i].push([rows])
+                    for(let j = 0; j < rows; j++){
+                        grid[i][j] = {
+                            'Cell' : 'cell_' + i + j,
+                            'Elem' : 'none'
+                        };
+                    }
                 }
+                this.grid = grid;
+                return grid;
+            },
 
-                // Return toutes les Cell avec leurs pos X et Y
-                SELECT ?Cell ?X ?Y
-                WHERE {
-                    ?Cell a grid:Cell .
-                    ?Cell grid:X ?X .
-                    ?Cell grid:Y ?Y .
-                }
+            build_grid(){
+              this.stardog_query('PlayerCell');
+              this.stardog_query('Ball1');
+              this.stardog_query('Ball2');
+              this.stardog_query('Ball3');
+            },
 
-                """*/
+            stardog_query(find){
+              let query_search = `
+                  SELECT ?Find ?X ?Y
+                  WHERE{
+                      ?Find a grid:`+find+` .
+                      ?Find grid:X ?X .
+                      ?Find grid:Y ?Y .
+                  }
+              `;
 
-                query.execute(conn, 'ontologie_db', request, 'application/sparql-results+json', {
-                    limit: 10,
-                    offset: 0,
-                    reasoning: true
-                }).then(({ body }) => {
-                    console.log(body.results.bindings);
-                    console.log(body.results.bindings[0].X.value);
-                    console.log(body.results.bindings[0].Y.value);
-                });
+              query.execute(conn, 'ontologie_db', query_search, 'application/sparql-results+json', {
+                  limit: 10,
+                  offset: 0,
+                  reasoning: true
+              }).then(({ body }) => {
+                  this.teste(find, body.results.bindings[0].X.value, body.results.bindings[0].Y.value);
+              });
+            },
+
+            teste(find, x, y){
+              this.grid[x][y].Elem = find;
+              console.log(this.grid[x][y]);
             }
         },
         computed: {
-            build_grid() {
-                let grid_div = document.querySelector('.grid');
-                let all_elem = grid_div.querySelectorAll('.elem');
-
-                for(let i = 0; i < all_elem.length; i++){
-                    all_elem[i].classList.add(this.grid[i].state);
-                }
-                return this.grid;
-            }
+            // remplir_grid() {
+            //     let grid_div = document.querySelector('.rows');
+            //     let all_elem = grid_div.querySelectorAll('.elem');
+            //
+            //     for(let i = 0; i < all_elem.length; i++){
+            //         all_elem[i].classList.add(this.grid[i].state);
+            //     }
+            //     return this.grid;
+            // }
         },
         data() {
             return {
                 test: "ici",
-                grid: [{
-                        id: 0,
-                        state: 'none'
-                    },
-                    {
-                        id: 1,
-                        state: 'top'
-                    },
-                    {
-                        id: 2,
-                        state: 'none'
-                    },
-                    {
-                        id: 3,
-                        state: 'player'
-                    },
-                    {
-                        id: 4,
-                        state: 'center'
-                    },
-                    {
-                        id: 5,
-                        state: 'none'
-                    },
-                    {
-                        id: 6,
-                        state: 'none'
-                    },
-                    {
-                        id: 7,
-                        state: 'bottom'
-                    },
-                    {
-                        id: 8,
-                        state: 'none'
-                    }
-                ]
+                grid: []
             }
         }
     }
@@ -149,23 +141,39 @@
 <style lang="css" scoped>
     .grid {
         display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
-        /* margin: 0 300px; */
+        grid-template-columns: repeat(10, 1fr);
     }
 
     .elem {
-        width: 200px;
-        height: 200px;
-        background: #777;
+        width: 60px;
+        height: 60px;
+        background: #6BB34D;
         border-radius: 5px;
         margin: 5px;
         background-image: url('../assets/nothing.png');
         background-position: center;
         background-repeat: no-repeat;
         background-size: cover;
+        transition: 1s;
+    }
+
+    .elem:hover {
+        background: #BF4040;
     }
 
     .moove div button {
+        width: 100px;
+        padding: 5px;
+        margin: 2px;
+        background: #333;
+        color: #DDD;
+        border: 2px solid #333;
+        border-radius: 2px;
+        font-weight: bold;
+        cursor: pointer;
+    }
+
+    button {
         width: 100px;
         padding: 5px;
         margin: 2px;
